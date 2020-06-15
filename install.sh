22d25280afb3626b37e278868b89a776b8b57dce
Yann Bolliger <yann.bolliger@sqooba.io>
2020-06-15 17:48:16 +0200

    Add installs to script, correct oh-my-zsh path.

--- install.sh
+++ install.sh
@@ -2,7 +2,35 @@
 
 PWD=$(pwd)
 
-ln -fhs "$PWD/zsh/.zshrc" ~/.zshrc
-ln -fhs "$PWD/zsh/custom/aliases.zsh" ~/.oh-my-zsh/custom/aliases.zsh
+# Install command line tools
+if [[ -z $(command -v xcode-select) ]]; then
+  xcode-select --install
+fi
 
-ln -fhs "$PWD/.prettierrc" ~/.prettierrc
+# Install brew
+if [[ -z $(command -v brew) ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Install first stuff with brew
+if [[ -z $(command -v autojump) ]]; then
+  brew install autojump rbenv nodenv
+fi
+
+if [[ -z $(command -v rbenv) ]]; then
+  brew install rbenv nodenv
+fi
+
+if [[ -z $(command -v nodenv) ]]; then
+  brew install nodenv
+fi
+
+# Install oh-my-zsh
+if [[ -z "$ZSH" ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+ln -fhs "$PWD/zsh/.zshrc" "$HOME/.zshrc"
+ln -fhs "$PWD/zsh/custom/aliases.zsh" "$ZSH/custom/aliases.zsh"
+
+ln -fhs "$PWD/.prettierrc" "$HOME/.prettierrc"
