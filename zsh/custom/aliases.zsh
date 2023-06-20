88c385b1858d937b361dfcd706413715177cc60c
Yann Bolliger <yann@bolliger.io>
2023-06-20 10:12:46 +0200

    Add branch top function

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,5 +1,10 @@
-
 alias gs="git status"
+
+function branch_top {
+    BRANCH=$(git_current_branch)
+    gcm && gl && gco $BRANCH && grbm
+}
+alias bt=branch_top
 
 # by Guillaume Tournigand
 function notify {
