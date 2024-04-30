58f8d0ec26c4d6d82e24a154e10e27fbd8b77d3f
Yann Bolliger <yann@bolliger.io>
2024-04-30 11:34:17 +0200

    Add approve many function

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -6,7 +6,18 @@ function branch_top {
 }
 alias bt=branch_top
 
-# by Guillaume Tournigand
+function approve_many {
+    if [ $# -ne 2 ]; then 
+        echo "approve_many <first pr id> <number of prs>"
+        return 1
+    fi
+    for i in {$1..$(($1 + $2 - 1))}; do 
+        gh pr review -a $i
+    done
+}
+alias apm=approve_many
+
+# by @ex0ns
 function notify {
     text="$@"
     osascript -e "display notification \"$text\" sound name \"Basso\""
